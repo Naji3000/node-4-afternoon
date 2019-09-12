d158b47a64295e91f6eec282a08ae4a912ad187a
Naji Ali <SupremeBinxx@MacBook-Pro.local>
2019-09-12 09:57:42 -0500

    finished

---
+++ server/middlewares/checkForSession.js
@@ -0,0 +1,12 @@
+
+module.exports = function (req, res, next ) {
+    if(!session.user) {
+        session.user = {
+        username: "",
+        cart: [], 
+        total: 0
+    }
+
+        next();
+    }
+}
